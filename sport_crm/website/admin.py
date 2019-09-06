9ba23b2bb7ab73a34910698a1f276b5dd2cf5b3d
Karl Zakhary <karlmaged33@gmail.com>
2019-09-06 03:01:34 +0200

    inlines for trainees in attendance

--- sport_crm/website/admin.py
+++ sport_crm/website/admin.py
@@ -3,10 +3,10 @@ from __future__ import unicode_literals
 from django import forms
 from django.contrib import admin
 from nested_inline.admin import NestedStackedInline, NestedModelAdmin
-from django.urls.base import resolve
+from django.urls import resolve
 from .models import *
 
-admin.site.site_header = "Matter Administration"
+admin.site.site_header = "Swimming School Administration"
 
 
 # class TraineeAdminForm(forms.ModelForm):
@@ -21,6 +21,42 @@ admin.site.site_header = "Matter Administration"
 #         self.fields['group'] = forms.ModelChoiceField(
 #             queryset=Class.objects.filter(level=trainee_instance.level))
 
+class AttendanceRecordAdminInline(admin.TabularInline):
+    extra = 0
+    model = AttendanceRecord
+    max_num = 6
+
+    # def __init__(self, obj=None, *args, **kwargs):
+    #     super(AttendanceRecordAdminInline, self).__init__(*args, **kwargs)
+    #     self.fields['trainee'].queryset = Trainee.objects.filter(group=obj)
+
+    def get_formset(self, request, obj=None, **kwargs):
+
+        if obj is None:
+            return super(AttendanceRecordAdminInline, self).get_formset(request, obj, **kwargs)
+        self.parent_obj = obj
+        extra = obj.trainees.all().count()
+        # registered = obj.trainees.get()
+        kwargs['extra'] = extra
+
+        return super(AttendanceRecordAdminInline, self).get_formset(request, obj, **kwargs)
+
+    # def get_parent_object_from_request(self, request):
+    #     resolved = resolve(request.path_info)
+    #
+    #     if resolved.args:
+    #         return self.parent_model.objects.get(pk=resolved.args[0])
+    #     return None
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "trainee":
+            group = self.parent_obj
+            if group:
+                kwargs["queryset"] = Trainee.objects.filter(group=group)
+
+        return super(AttendanceRecordAdminInline, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
+
 
 class TraineeAdmin(admin.ModelAdmin):
     model = Trainee
@@ -28,29 +64,36 @@ class TraineeAdmin(admin.ModelAdmin):
     # form = TraineeAdminForm
     list_display = ('name', 'phone_number', 'group', 'level', 'reference')
     search_fields = ('reference',)
-    def __init__(self, *args, **kwargs):
-        super(TraineeAdmin, self).__init__(*args, **kwargs)
-        self.reference = str(uuid.uuid4())
+    inlines = (AttendanceRecordAdminInline,)
+
+    # def __init__(self, *args, **kwargs):
+    #     super(TraineeAdmin, self).__init__(*args, **kwargs)
+    #     self.reference = str(uuid.uuid4())
 
 
-    def get_parent_object_from_request(self, request):
-        resolved = resolve(request.path_info)
-        if resolved.args:
-            return self.parent_model.objects.get(pk=resolved.args[0])
-        return None
+    # def get_parent_object_from_request(self, request):
+    #     resolved = resolve(request.path_info)
+    #     if resolved.args:
+    #         return self.parent_model.objects.get(pk=resolved.args[0])
+    #     return None
+    #
+    # def formfield_for_foreignkey(self, db_field, request, **kwargs):
+    #     if db_field.name == "group":
+    #         trainee = self.get_parent_object_from_request(request)
+    #         if trainee:
+    #             kwargs["queryset"] = Class.objects.filter(level=trainee.level)
+    #
+    #     return super(TraineeAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == "group":
-            trainee = self.get_parent_object_from_request(request)
-            if trainee:
-                kwargs["queryset"] = Class.objects.filter(level=trainee.level)
-        return super(TraineeAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
 class TrainerAdmin(admin.ModelAdmin):
     model = Trainer
 
+
 class ClassAdmin(admin.ModelAdmin):
     model = Class
+    inlines = (AttendanceRecordAdminInline,)
+
 
 class AttendanceRecordAdmin(admin.ModelAdmin):
     model = AttendanceRecord
--- sport_crm/website/models.py
+++ sport_crm/website/models.py
@@ -12,12 +12,22 @@ LEVEL_CHOICES = (
                     )
 
 # Models
+
+
 class Trainer(models.Model):
     name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Class(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Groups"
+        verbose_name = "Group"
+
     name = models.CharField(max_length=255)
     trainer = models.ForeignKey(Trainer, related_name="trainers", on_delete=models.CASCADE)
     level = models.CharField(max_length=1, choices=LEVEL_CHOICES, null=True, blank=True)
@@ -25,7 +35,9 @@ class Class(models.Model):
     def __str__(self):
         return self.name
 
+
 class Trainee(models.Model):
+
     name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=255)
     registered_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
@@ -36,6 +48,7 @@ class Trainee(models.Model):
     # def __init__(self, *args, **kwargs):
     #     self.reference = str(uuid.uuid4())
     #     super(Trainee, self).__init__(self, *args, **kwargs)
+
     def save(self, *args, **kwargs):
         if not self.reference:
             self.reference = str(uuid.uuid4())[:5]
@@ -45,6 +58,9 @@ class Trainee(models.Model):
     def __unicode__(self):
         return self.name
 
+    def __str__(self):
+        return "{}, id:{}".format(self.name,self.reference)
+
 
 class AttendanceRecord(models.Model):
     group = models.ForeignKey(Class, related_name="records", on_delete=models.CASCADE)
@@ -52,5 +68,7 @@ class AttendanceRecord(models.Model):
     ATTENDANCE_TYPES = (
         ('present', 'Present'),
         ('absent', 'Absent'),
+        ('excuse', 'Excuse')
     )
     status = models.CharField(max_length=10, choices=ATTENDANCE_TYPES)
+    registered_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
